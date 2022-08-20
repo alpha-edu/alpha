7e299edcee71b6dc3626335e88f90edaa4041743
namishk <namishkhandelwal1@gmail.com>
2022-08-20 12:16:09 +0530

    colorscheme modify

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "dark-gray": "#0f0e0f",
+        "soft-pink": "#f0ebef",
+        "light-gray": "#97a0a7",
+        "purple-dim": "#756a77",
+        "purple-mid": "#c6bbc9",
+        "soft-brown": "#787572",
+        "bright-purple": "#cc9cd7",
+        "blue-smoke": "#698780",
+        "pink-dirty": "#bfb1a8",
+      }
+    },
   },
   plugins: [],
 }
